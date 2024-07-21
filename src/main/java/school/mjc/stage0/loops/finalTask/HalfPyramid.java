747c1b541b21be9b91a2750f1a6c881331fa45cb
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-07-21 13:16:21 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,33 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int temp = 1;
+        int tempI;
 
+        for (int i = 1; i < cathetusLength-2; i++) {
+            for (int j = 1; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            tempI = i;
+            boolean flag = true;
+            for (int j = 1; j <= temp; j++) {
+
+                if (tempI == 1) flag = false;
+                if (flag) {
+                    System.out.print("*");
+                    tempI--;
+                } else {
+                    System.out.print(" ");
+                    tempI++;
+                }
+            }
+            temp += 2;
+
+            for (int j = 1; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,31 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int temp = 1;
+        int tempI;
 
+        for (int i = 1; i < cathetusLength-1; i++) {
+            for (int j = 1; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            tempI = i;
+            boolean flag = true;
+            for (int j = 1; j <= temp; j++) {
+                System.out.print(tempI);
+                if (tempI == 1) flag = false;
+                if (flag) {
+                    tempI--;
+                } else {
+                    tempI++;
+                }
+            }
+            temp += 2;
+
+            for (int j = 1; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
