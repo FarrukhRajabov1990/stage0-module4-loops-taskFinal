302f67e9b3e598e3f9becfb2d1dfbb441ba67dd5
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-07-21 13:29:52 +0500

    Third commit

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+
         int result;
         for (int i = 1; i <= 10; i++) {
             result = i * numberTableToPrint;
